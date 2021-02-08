c27e2aaba5ae22dcc7313c2f8fa33938c492a075
ntoniocp <antoniojcp96@gmail.com>
2021-02-07 21:38:13 -0400

    Tabulation fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const {nexmo, PORT} = require('./config/config');
+const { nexmo, PORT } = require('./config/config');
 
 const app = express();
 const from = 'SMS Hello World';
@@ -12,24 +12,26 @@ app.use(cors());
 app.use('/', express.static(__dirname + '/public'));
 
 app.post('/sms', express.json(), (req, res) => {
-    const to = req.body.phone;
-    let message = req.body.message;
+  const to = req.body.phone;
+  let message = req.body.message;
 
-    if (to && message) {
-        message = message.slice(0, 67);
-        
-        nexmo.message.sendSms(from, to, message, (err, responseData) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({msg: 'Error en envio de mensaje'});
-            } else {
-                console.log(responseData);
-                res.send({msg: 'Mensaje enviado'});
-            }
-        });
-    } else {
-        res.status(400).send({msg: 'Parametros invalidos'});
-    }
+  if (to && message) {
+    message = message.slice(0, 67);
+
+    nexmo.message.sendSms(from, to, message, (err, responseData) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(500)
+          .send({ msg: 'Error has ocurred when trying to send msg' });
+      } else {
+        console.log(responseData);
+        res.send({ msg: 'Message sent' });
+      }
+    });
+  } else {
+    res.status(400).send({ msg: 'Invalid parameters' });
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- public/messaging.js
+++ public/messaging.js
@@ -5,28 +5,28 @@ const sendBtn = document.querySelector('#sendMessageBtn');
 sendBtn.addEventListener('click', sendMessage);
 
 function sendMessage(event) {
-    if (phoneNumber.value && message.value) {
-        const url = '/sms';
-        const data = {
-            phone : onlyNumbers(phoneNumber.value),
-            message: message.value  
-        };
+  if (phoneNumber.value && message.value) {
+    const url = '/sms';
+    const data = {
+      phone: onlyNumbers(phoneNumber.value),
+      message: message.value,
+    };
 
-        const requestOptions =  {
-            method: 'POST', 
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-        
-        fetch(url, requestOptions)
-            .then(res => res.text())
-            .then(jsonRes => console.log('Success:', jsonRes))
-            .catch(error => console.error('Error:', error));
-    }
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(url, requestOptions)
+      .then((res) => res.text())
+      .then((jsonRes) => console.log('Success:', jsonRes))
+      .catch((error) => console.error('Error:', error));
+  }
 }
 
 function onlyNumbers(string) {
-    return string.replace(/\D/g,''); 
+  return string.replace(/\D/g, '');
 }
